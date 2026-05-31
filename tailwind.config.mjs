7c0410059588841c0a6257db76ac8f1f81a4daaf
Nyoman Sinar <sinar.bonar@gmail.com>
2026-05-31 13:21:31 +0800

    init: The Bali Trip Tour static site

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#426235',
+          light: '#576e4d',
+          dark: '#243b1d',
+          button: '#345126',
+        },
+        sub: '#607263',
+        subtext: '#383e38',
+        text: '#1f211d',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Noto Sans TC', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
